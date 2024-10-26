294939ab39a535f31d037c0c0f060fbaee0e356d
Krzysztof Wyrzykowski <wyrzykowski92@gmail.com>
2024-10-26 13:09:24 +0200

    Functions to test added

---
+++ functions.py
@@ -0,0 +1,18 @@
+from cec2017.simple import f3, f7
+
+
+def quadratic(params):
+    return sum([x**2 for x in params])
+
+
+def my_f3(params):
+    return f3([params])[0]
+
+
+def my_f7(params):
+    return f7([params])[0]
+
+
+print(quadratic([10.0 for _ in range(10)]))
+print(my_f3([10.0 for _ in range(10)]))
+print(my_f7([10.0 for _ in range(10)]))
